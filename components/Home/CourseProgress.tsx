9429a082fb91668d8527f117aba123f9eac5d795
Kingsley Eneja <kingsley.eneja@getir.com>
2025-06-28 15:49:48 -0300

    Refactor progress card

--- components/Home/CourseProgress.tsx
+++ components/Home/CourseProgress.tsx
@@ -1,10 +1,8 @@
-import { colors } from '@/constants/colors';
-import { images } from '@/constants/images';
-import { ICourseChapter } from '@/types/course';
+import { ICourse } from '@/types/course';
 import { DocumentData } from 'firebase/firestore';
 import React from 'react';
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
-import * as Progress from 'react-native-progress';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import ProgressCard from '../Shared/ProgressCard';
 
 interface Props {
   courseList: DocumentData[];
@@ -19,25 +17,11 @@ const CourseProgress = ({ courseList }: Props) => {
         data={courseList}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item, index }) => {
-          const completed = item.courseChapters.filter((ch: ICourseChapter) => !!ch.completedAt).length;
-          const progress = completed / item.courseChapters.length;
-          return ((
-            <View style={styles.progressCard} key={index}>
-              <View style={styles.progressCardFlex}>
-                <Image source={images.appIcon} style={styles.bannerImg} />
-                <View style={styles.courseInfoContainer}>
-                  <Text numberOfLines={2} style={styles.cardCourseTitle}>{item?.courseTitle}</Text>
-                  <Text style={styles.cardCourseChapterCount}>{item?.courseChapters.length} Chapters</Text>
-                </View>
-              </View>
-              <View style={styles.progressBarContainer}>
-                <Progress.Bar progress={progress} width={250} color={colors.PRIMARY_BLUE} />
-                <Text style={styles.progessBarText}>Completed {completed} / {item.courseChapters.length} Chapters</Text>
-              </View>
-            </View>
-          ));
-        }}
+        renderItem={({ item, index }) => (
+          <View key={index}>
+            <ProgressCard course={item as ICourse} />
+          </View>
+        )}
       />
     </View>
   );
@@ -53,40 +37,4 @@ const styles = StyleSheet.create({
     fontFamily: 'roboto-bold',
     fontSize: 25,
   },
-  progressCard: {
-    width: 280,
-    margin: 7,
-    padding: 15,
-    backgroundColor: colors.BG_GRAY,
-    borderRadius: 8,
-  },
-  progressCardFlex: {
-    display: 'flex',
-    flexDirection: 'row',
-    gap: 8,
-  },
-  bannerImg: {
-    width: 80,
-    height: 80,
-    borderRadius: 8,
-    backgroundColor: colors.DARK_BLUE,
-  },
-  courseInfoContainer: {
-    flex: 1,
-  },
-  cardCourseTitle: {
-    fontFamily: 'roboto-bold',
-    fontSize: 19,
-  },
-  cardCourseChapterCount: {
-    fontFamily: 'roboto',
-    fontSize: 16
-  },
-  progressBarContainer: {
-    marginTop: 10,
-  },
-  progessBarText: {
-    fontFamily: 'roboto',
-    marginTop: 2,
-  }
 });
---
+++ components/Shared/ProgressCard.tsx
@@ -0,0 +1,67 @@
+import { colors } from '@/constants/colors';
+import { images } from '@/constants/images';
+import { ICourse, ICourseChapter } from '@/types/course';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import * as Progress from 'react-native-progress';
+
+const ProgressCard = ({ course }: { course: ICourse; }) => {
+  const completed = course.courseChapters.filter((ch: ICourseChapter) => !!ch.completedAt).length;
+  const progress = completed / course.courseChapters.length;
+  return (
+    <View style={styles.progressCard}>
+      <View style={styles.progressCardFlex}>
+        <Image source={images.appIcon} style={styles.bannerImg} />
+        <View style={styles.courseInfoContainer}>
+          <Text numberOfLines={2} style={styles.cardCourseTitle}>{course?.courseTitle}</Text>
+          <Text style={styles.cardCourseChapterCount}>{course?.courseChapters.length} Chapters</Text>
+        </View>
+      </View>
+      <View style={styles.progressBarContainer}>
+        <Progress.Bar progress={progress} width={250} color={colors.PRIMARY_BLUE} />
+        <Text style={styles.progessBarText}>Completed {completed} / {course.courseChapters.length} Chapters</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ProgressCard;
+
+const styles = StyleSheet.create({
+  progressCard: {
+    width: 280,
+    margin: 7,
+    padding: 15,
+    backgroundColor: colors.BG_GRAY,
+    borderRadius: 8,
+  },
+  progressCardFlex: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 8,
+  },
+  bannerImg: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+    backgroundColor: colors.DARK_BLUE,
+  },
+  courseInfoContainer: {
+    flex: 1,
+  },
+  cardCourseTitle: {
+    fontFamily: 'roboto-bold',
+    fontSize: 19,
+  },
+  cardCourseChapterCount: {
+    fontFamily: 'roboto',
+    fontSize: 16
+  },
+  progressBarContainer: {
+    marginTop: 10,
+  },
+  progessBarText: {
+    fontFamily: 'roboto',
+    marginTop: 2,
+  }
+});
